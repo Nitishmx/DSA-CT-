a3dce0244d5521a76da5f5de86985aa08a4dc334
nitishmx <nitish7739879577@gmail.com>
2024-07-02 18:10:42 +0530

    product sum

--- BasiceDSA/print.java
+++ BasiceDSA/print.java
@@ -1,9 +1,9 @@
 public class print {
     public static void main(String[] args) {
-        int array[] = { 10, 40, 20, 3, 0, 10 };
-        int sum = 0;
+        int array[] = { 10, 40, 20, 3, 10, 10 };
+        int sum = 1;
         for (int i = 0; i < array.length; i++) {
-            sum = sum + array[i];
+            sum = sum * array[i];
         }
         System.out.println(sum);
     }
